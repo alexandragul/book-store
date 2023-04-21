e19d8cd5dab0999fa0d00d25bdf95f722c49227c
alexandragul <alexandra.ggul@gmail.com>
2023-04-21 09:52:01 +0300

    Set up MUI theme

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,17 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
+import theme, { roboto } from "../theme";
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html lang="en" className={roboto.className}>
+      <Head>
+        <meta name="theme-color" content={theme.palette.primary.main} />
+        <link rel="shortcut icon" href="/favicon.ico" />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
