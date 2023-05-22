82db03a2edf7251ef89f2989849a1858060f389f
alexandragul <alexandra.ggul@gmail.com>
2023-05-22 17:02:45 +0300

    Configure button in theme

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -1,18 +1,18 @@
-import { Roboto } from "next/font/google";
-import { createTheme } from "@mui/material/styles";
+import { Roboto } from "next/font/google"
+import { createTheme } from "@mui/material/styles"
 
 export const roboto = Roboto({
   weight: ["300", "400", "500", "700"],
   subsets: ["latin"],
   display: "swap",
   fallback: ["Helvetica", "Arial", "sans-serif"],
-});
+})
 
 // Create a theme instance.
 const theme = createTheme({
   palette: {
     primary: {
-      main: "#2f3457",
+      main: "#6e50d5",
     },
     secondary: {
       main: "#b9cc4c",
@@ -21,6 +21,17 @@ const theme = createTheme({
   typography: {
     fontFamily: roboto.style.fontFamily,
   },
-});
+  components: {
+    MuiButton: {
+      styleOverrides: {
+        root: {
+          borderRadius: "20px",
+          borderWidth: "2px",
+          boxShadow: "none",
+        },
+      },
+    },
+  },
+})
 
-export default theme;
+export default theme
