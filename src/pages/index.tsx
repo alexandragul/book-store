d9c3dee3c634e760df10d10d1dac5c2a9d09ff41
alexandragul <alexandra.ggul@gmail.com>
2023-05-23 19:16:05 +0300

    Set up basic hero

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import Head from "next/head"
 import Layout from "@/components/Layout"
+import Hero from "@/components/pages/index/Hero"
 
 export default function Home() {
   return (
@@ -10,7 +11,9 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Layout></Layout>
+      <Layout>
+        <Hero />
+      </Layout>
     </>
   )
 }
