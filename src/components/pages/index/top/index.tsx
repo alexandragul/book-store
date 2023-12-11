d96bdf2566fdd438185df61c8d163cea7819894c
alexandragul <alexandra.ggul@gmail.com>
2023-12-11 18:51:30 +0300

    Fix mobile responsiveness for top section

--- src/components/pages/index/top/index.tsx
+++ src/components/pages/index/top/index.tsx
@@ -22,7 +22,7 @@ const BooksTop: FC<{ books: Book[] }> = ({ books }) => {
 
 const AuthorsTop: FC<{ authors: Author[] }> = ({ authors }) => {
   return (
-    <Box bgcolor="white" borderRadius="16px" padding={3} my={-3}>
+    <Box bgcolor="white" borderRadius="16px" padding={3} my={[0, -3]}>
       <Grid container>
         <Grid item mb={1}>
           <Typography variant="h4">Top 6 authors</Typography>
@@ -40,10 +40,10 @@ const AuthorsTop: FC<{ authors: Author[] }> = ({ authors }) => {
 const Top: FC<{ books: Book[] }> = ({ books }) => {
   return (
     <Grid container>
-      <Grid item xs={6} md={8}>
+      <Grid item xs={12} sm={6} md={8}>
         <BooksTop books={books} />
       </Grid>
-      <Grid item xs={6} md={4} direction="column">
+      <Grid item xs={12} sm={6} md={4} direction="column">
         <AuthorsTop authors={authorsTop} />
       </Grid>
     </Grid>
